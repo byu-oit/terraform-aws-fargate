57dc7e350bfa347c9e54051a9df08623acfe1a25
Scott Hutchings <yoshutch@gmail.com>
2019-10-17 11:00:41 -0600

    added region to provider and added specific outputs

--- fargate-service/main.tf
+++ fargate-service/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-
+  region = "us-west-2"
 }
 
 data "aws_caller_identity" "current" {}
--- fargate-service/output.tf
+++ fargate-service/output.tf
@@ -1,3 +1,6 @@
-output "fargate_service" {
-  value = aws_ecs_service.service
+output "fargate_service_name" {
+  value = aws_ecs_service.service.name
+}
+output "fargate_cluster_name" {
+  value = aws_ecs_cluster.cluster.name
 }
